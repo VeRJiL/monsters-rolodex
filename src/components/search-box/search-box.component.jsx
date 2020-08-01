d925bd5c99c4c3be474c74d20c19ef328451723c
VeRJiL <arash.nykbakht@gmail.com>
2020-08-01 06:38:43 +0430

    add porject

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './search-box.style.css';
+
+export const SearchBox = ({ placeHolder, handlechange }) => (
+    <input 
+        className="search"
+        type="search" 
+        placeholder={ placeHolder } 
+        onChange={ handlechange } 
+    />
+);
